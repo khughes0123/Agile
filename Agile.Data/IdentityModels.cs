b261cb89854a2264282e663c2a3aa4932f5fabd1
Ashley Lawrence <ashleylawrence836@mail.com>
2021-03-04 10:29:58 -0500

    added Show class

--- Agile.Data/IdentityModels.cs
+++ Agile.Data/IdentityModels.cs
@@ -34,6 +34,7 @@ namespace Agile.Data
         }
 
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<Show> Shows { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ Agile.Models/ShowCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.Models
+{
+    public class ShowCreate
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Season { get; set; }
+        public int Episode { get; set; }
+    }
+}
